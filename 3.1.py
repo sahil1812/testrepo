28a3371aef76ab752b1831699c0f856b57236756
sahil1812 <80686259+sahil1812@users.noreply.github.com>
2021-06-07 19:53:24 +0530

    Add files via upload

---
+++ 3.1.py
@@ -0,0 +1,18 @@
+'''1) Write a program to demonstrate the use of keyword and default arguments.'''
+def to_buy(item,price=100):
+    print(item,price)
+to_buy(item='mouse',price=650)
+to_buy(item='mousepad',price=110)
+to_buy(item='cover')
+
+
+
+
+
+
+
+
+
+
+
+ 
